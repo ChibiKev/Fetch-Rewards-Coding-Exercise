bf3c13303204ac9afdc955116488bdf04f7ace68
ChibiKev <k3v.ch3n@gmail.com>
2021-12-05 16:12:42 -0500

    Added Transactions DB Model

--- models/Transactions.py
+++ models/Transactions.py
@@ -3,7 +3,11 @@ from app import db
 
 class Transactions(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  content = db.Column(db.String(100), unique=False, nullable=False)
+  payer = db.Column(db.String(100), unique=False, nullable=False)
+  points = db.Column(db.Integer(), unique=False, nullable=False)
+  timestamp = db.Column(db.DateTime(), unique=False, nullable=False)
 
-  def __init__(self, content):
-    self.content = content
+  def __init__(self, payer, points, timestamp):
+    self.payer = payer
+    self.points = points
+    self.timestamp = timestamp
